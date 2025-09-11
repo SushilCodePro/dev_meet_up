71c3ced4c82c49fc1112480044a3fb6b3b36ac22
Sushil <sushil.codepro@gmail.com>
2025-09-11 15:18:44 +0530

    Implement log out

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -54,3 +54,20 @@ export const signin = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+//logout
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: false, // only secure in prod // process.env.NODE_ENV === "production",
+      sameSite: "strict", // prevent CSRF
+    });
+// secure: false → the cookie will also be sent over HTTP (in local development).
+// secure: true → the cookie will only be sent by the browser over HTTPS (not HTTP).
+    return res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
