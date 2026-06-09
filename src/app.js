2e216af95b8ea6e0b5d2d037b39897cd7d4945ad
Sushil Gupta <sushil.codepro@gmail.com>
2026-06-09 16:15:13 +0530

    cors allow

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ const app = express();
 app.set("trust proxy", 1);
 // Middlewares
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["https://developersadda.netlify.app","http://localhost:5173"],
+  // origin: "http://localhost:5173",
   credentials: true
 }));
 app.use(express.json());
