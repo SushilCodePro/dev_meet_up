d8ec0d02bb2c5ed8a76ed728c37d523b0f46bbc0
Sushil Gupta <sushil.codepro@gmail.com>
2026-09-11 09:34:44 +0530

    fix- Verify JWT signature first then block token in redis

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -9,15 +9,15 @@ export const verifyToken = async (req, res, next) => {
   }
 
   try {
-    // 1. Check if token is blocked in Redis
+    // 1. Verify JWT signature with ACCESS secret
+    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+    console.log('decoded user 20', decoded);
+
+    // 2. Check if token is blocked in Redis
     const IsBlocked = await redisClient.exists(`accessToken-${token}`);
     if (IsBlocked) {
       return res.status(401).json({ message: "Access denied. Token is blacklisted." });
     }
-
-    // 2. Verify JWT signature with ACCESS secret
-    const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
-    console.log('decoded user 20', decoded);
 
     req.user = decoded; // attach user info to request
     next();
